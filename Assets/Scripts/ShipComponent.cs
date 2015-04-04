6732253968565c24acc930012f84c216a0e6ce46
Jay Gloster <jgloster@du.edu>
2015-04-04 14:19:27 -0600

    Playing with setting up ship components

---
+++ Assets/Scripts/ShipComponent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipComponent : MonoBehaviour {
+
+    public Ship parentShip;
+	// Use this for initialization
+	void Start () {
+        gameObject.transform.SetParent(parentShip.gameObject.transform);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
