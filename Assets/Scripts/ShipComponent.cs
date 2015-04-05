f64b74252377ad46607593c1881023e88c2c11c3
Nathan Clarke <CaptainCow95@gmail.com>
2015-04-05 15:59:17 -0600

    Adding basic UI and ship and ship componenet prefabs

--- Assets/Scripts/ShipComponent.cs
+++ Assets/Scripts/ShipComponent.cs
@@ -1,16 +1,47 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class ShipComponent : MonoBehaviour {
-
+public class ShipComponent : MonoBehaviour
+{
+    public Direction direction;
+    public Vector2 offset;
     public Ship parentShip;
-	// Use this for initialization
-	void Start () {
+
+    public enum Direction
+    {
+        Up,
+        Left,
+        Down,
+        Right
+    }
+
+    // Use this for initialization
+    private void Start()
+    {
         gameObject.transform.SetParent(parentShip.gameObject.transform);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        gameObject.transform.localPosition = offset;
+
+        switch (direction)
+        {
+            case Direction.Up:
+                gameObject.transform.localRotation = Quaternion.Euler(0, 0, 0);
+                break;
+
+            case Direction.Left:
+                gameObject.transform.localRotation = Quaternion.Euler(0, 0, 90);
+                break;
+
+            case Direction.Down:
+                gameObject.transform.localRotation = Quaternion.Euler(0, 0, 180);
+                break;
+
+            case Direction.Right:
+                gameObject.transform.localRotation = Quaternion.Euler(0, 0, 270);
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
 }
