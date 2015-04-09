a6fb28262e80809bad688e40815fee0e8f0d3649
Nathan Clarke <CaptainCow95@gmail.com>
2015-04-09 01:43:13 -0600

    Adding namespaces to files that are missing them

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -1,80 +1,83 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class Asteroid : MonoBehaviour
+namespace AsteroidBelt
 {
-    public float mineralRating;
-    public Vector2[] newUV;
-    public int numberOfVertices;
-    public float radius;
-    private int[] newTriangles;
-    private Vector3[] newVertices;
-
-    private void CreateMesh()
+    public class Asteroid : MonoBehaviour
     {
-        CreateVertices();
-        CreateTriangles();
-        Mesh mesh = new Mesh();
-        GetComponent<MeshFilter>().mesh = mesh;
-        mesh.vertices = newVertices;
-        mesh.uv = newUV;
-        mesh.triangles = newTriangles;
+        public float mineralRating;
+        public Vector2[] newUV;
+        public int numberOfVertices;
+        public float radius;
+        private int[] newTriangles;
+        private Vector3[] newVertices;
 
-        PolygonCollider2D collider = GetComponent<PolygonCollider2D>();
-
-        // vertice count +1 for polygon closure and -1 for removing the 0, 0 point
-        Vector2[] points = new Vector2[newVertices.Length];
-        for (int i = 1; i < newVertices.Length; i++)
+        private void CreateMesh()
         {
-            points[i - 1] = newVertices[i];
+            CreateVertices();
+            CreateTriangles();
+            Mesh mesh = new Mesh();
+            GetComponent<MeshFilter>().mesh = mesh;
+            mesh.vertices = newVertices;
+            mesh.uv = newUV;
+            mesh.triangles = newTriangles;
+
+            PolygonCollider2D collider = GetComponent<PolygonCollider2D>();
+
+            // vertice count +1 for polygon closure and -1 for removing the 0, 0 point
+            Vector2[] points = new Vector2[newVertices.Length];
+            for (int i = 1; i < newVertices.Length; i++)
+            {
+                points[i - 1] = newVertices[i];
+            }
+
+            points[points.Length - 1] = points[0];
+            collider.points = points;
         }
 
-        points[points.Length - 1] = points[0];
-        collider.points = points;
-    }
-
-    private void CreateTriangles()
-    {
-        List<int> tris = new List<int>();
-        for (int i = 2; i < newVertices.Length; ++i)
+        private void CreateTriangles()
         {
+            List<int> tris = new List<int>();
+            for (int i = 2; i < newVertices.Length; ++i)
+            {
+                tris.Add(0);
+                tris.Add(i - 1);
+                tris.Add(i);
+            }
+
             tris.Add(0);
-            tris.Add(i - 1);
-            tris.Add(i);
+            tris.Add(newVertices.Length - 1);
+            tris.Add(1);
+            newTriangles = tris.ToArray();
         }
 
-        tris.Add(0);
-        tris.Add(newVertices.Length - 1);
-        tris.Add(1);
-        newTriangles = tris.ToArray();
-    }
-
-    private void CreateVertices()
-    {
-        List<Vector3> vertices = new List<Vector3> { new Vector3(0, 0, 0) };
-        float theta = (2f * Mathf.PI);
-        while (theta > 0 + (Mathf.Epsilon))
+        private void CreateVertices()
         {
-            float randRad = Random.Range(radius * .90f, radius * 1.10f);
-            float x = randRad * Mathf.Cos(theta);
-            float y = randRad * Mathf.Sin(theta);
-            vertices.Add(new Vector3(x, y));
-            theta -= (2f * Mathf.PI) / numberOfVertices;
+            List<Vector3> vertices = new List<Vector3> { new Vector3(0, 0, 0) };
+            float theta = (2f * Mathf.PI);
+            while (theta > 0 + (Mathf.Epsilon))
+            {
+                float randRad = Random.Range(radius * .90f, radius * 1.10f);
+                float x = randRad * Mathf.Cos(theta);
+                float y = randRad * Mathf.Sin(theta);
+                vertices.Add(new Vector3(x, y));
+                theta -= (2f * Mathf.PI) / numberOfVertices;
+            }
+
+            newVertices = vertices.ToArray();
         }
 
-        newVertices = vertices.ToArray();
-    }
+        // Use this for initialization
+        private void Start()
+        {
+            CreateMesh();
+        }
 
-    // Use this for initialization
-    private void Start()
-    {
-        CreateMesh();
-    }
-
-    // Update is called once per frame
-    private void Update()
-    {
-        float scale = mineralRating / 100f;
-        gameObject.transform.localScale = new Vector3(scale, scale, 1);
+        // Update is called once per frame
+        private void Update()
+        {
+            float scale = mineralRating / 100f;
+            gameObject.transform.localScale = new Vector3(scale, scale, 1);
+        }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,29 +6,29 @@ namespace AsteroidBelt
 {
     public class GameManager : Singleton<GameManager>
     {
-        public GameObject asteroid;
-        public GameObject emptyShip;
-        public GameObject[] possibleShipComponents;
+        public GameObject asteroidPrefab;
+        public GameObject[] shipComponentPrefabs;
+        public GameObject shipPrefab;
 
         public void CreateAsteroid(Vector2 position, float radius, int numberOfVertices, float mineralRating)
         {
-            GameObject newAsteroid = Instantiate(asteroid, position, Quaternion.identity) as GameObject;
-            Asteroid asteroidComponent = newAsteroid.GetComponent<Asteroid>();
-            asteroidComponent.radius = radius;
-            asteroidComponent.numberOfVertices = numberOfVertices;
-            asteroidComponent.mineralRating = mineralRating;
+            GameObject asteroidObject = Instantiate(asteroidPrefab, position, Quaternion.identity) as GameObject;
+            Asteroid asteroid = asteroidObject.GetComponent<Asteroid>();
+            asteroid.radius = radius;
+            asteroid.numberOfVertices = numberOfVertices;
+            asteroid.mineralRating = mineralRating;
         }
 
         public void CreateShip(Vector2 position, Vector2[] componentPositions, ShipComponent.Direction[] componentDirections, int[] shipComponents, bool playerControlled)
         {
-            GameObject newShip = Instantiate(emptyShip, position, Quaternion.identity) as GameObject;
-            Ship ship = newShip.GetComponent<Ship>();
+            GameObject shipObject = Instantiate(shipPrefab, position, Quaternion.identity) as GameObject;
+            Ship ship = shipObject.GetComponent<Ship>();
             ship.playerControlled = playerControlled;
             for (int i = 0; i < shipComponents.Length; ++i)
             {
-                GameObject newShipComponent = Instantiate(possibleShipComponents[shipComponents[i]], componentPositions[i], Quaternion.identity) as GameObject;
+                GameObject newShipComponent = Instantiate(shipComponentPrefabs[shipComponents[i]], componentPositions[i], Quaternion.identity) as GameObject;
                 ShipComponent comp = newShipComponent.GetComponent<ShipComponent>();
-                comp.parentShip = newShip;
+                comp.parentShip = shipObject;
                 comp.direction = componentDirections[i];
                 ship.addShipComponent(comp);
             }
