2acf1ed910115fecf0f7fb917290856d1fbae6cf
Nathan Clarke <CaptainCow95@gmail.com>
2015-04-08 23:58:13 -0600

    Fixing how asteroids are rendered

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -21,31 +21,37 @@ public class Asteroid : MonoBehaviour
         mesh.triangles = newTriangles;
 
         PolygonCollider2D collider = GetComponent<PolygonCollider2D>();
-        Vector2[] points = new Vector2[(newVertices.Length / 2) + 1];
-        for (int i = 0; i < newVertices.Length / 2; i++)
+
+        // vertice count +1 for polygon closure and -1 for removing the 0, 0 point
+        Vector2[] points = new Vector2[newVertices.Length];
+        for (int i = 1; i < newVertices.Length; i++)
         {
             points[i] = newVertices[i];
         }
-        points[points.Length] = newVertices[0];
+
+        points[points.Length] = points[0];
         collider.points = points;
     }
 
     private void CreateTriangles()
     {
         List<int> tris = new List<int>();
-        for (int i = 1; i < newVertices.Length; ++i)
+        for (int i = 2; i < newVertices.Length; ++i)
         {
             tris.Add(0);
+            tris.Add(i - 1);
             tris.Add(i);
-            if (i + 1 < newVertices.Length) tris.Add(i + 1);
         }
+
         tris.Add(0);
+        tris.Add(newVertices.Length - 1);
+        tris.Add(1);
         newTriangles = tris.ToArray();
     }
 
     private void CreateVertices()
     {
-        List<Vector3> vertices = new List<Vector3>();
+        List<Vector3> vertices = new List<Vector3> { new Vector3(0, 0, 0) };
         float theta = (2f * Mathf.PI);
         while (theta > 0 + (Mathf.Epsilon))
         {
@@ -53,8 +59,9 @@ public class Asteroid : MonoBehaviour
             float x = randRad * Mathf.Cos(theta);
             float y = randRad * Mathf.Sin(theta);
             vertices.Add(new Vector3(x, y));
-            theta -= (2f * Mathf.PI) / (float)numberOfVertices;
+            theta -= (2f * Mathf.PI) / numberOfVertices;
         }
+
         newVertices = vertices.ToArray();
     }
 
--- Assets/Scripts/LoadLevel1.cs
+++ Assets/Scripts/LoadLevel1.cs
@@ -13,7 +13,7 @@ namespace AsteroidBelt
         private void Awake()
         {
             GameManager.Instance.CreateShip(samplePosition, samplePositions, sampleDirections, sampleComponents, true);
-            GameManager.Instance.CreateAsteroid(new Vector2(5.0f, 0f), 3, 16, 300);
+            GameManager.Instance.CreateAsteroid(new Vector2(5.0f, 0f), 1, 16, 300);
         }
     }
 }
