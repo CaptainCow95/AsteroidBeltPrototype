1b1bc29eab608e4075a8e5df9da12ef7912db5bb
Jay Gloster <jgloster@du.edu>
2015-05-02 20:34:59 -0600

    Mining asteroids now adds ore to inventory

---
+++ Assets/Scripts/InventoryUI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+using UnityEngine.UI;
+
+namespace AsteroidBelt
+{
+    public class InventoryUI : MonoBehaviour
+    {
+        private Inventory inventory;
+        private GameObject playerShip;
+        private Text text;
+
+        // Update is called once per frame
+        private void Update()
+        {
+            playerShip = GameObject.FindGameObjectWithTag("PlayerShip");
+            text = gameObject.GetComponentInChildren<Text>();
+            inventory = playerShip.GetComponent<Ship>().inventory;
+            text.text = "Inventory: \n" + inventory.ToString();
+        }
+    }
+}
