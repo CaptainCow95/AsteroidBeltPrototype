e488152666c251d043a921626198cb76e9721e78
Jay Gloster <jgloster@du.edu>
2015-05-29 11:56:31 -0600

    Added instructions

---
+++ Assets/Scripts/ToggleInstructions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ToggleInstructions : MonoBehaviour
+{
+    public GameObject instructions;
+
+    public void toggleActive()
+    {
+        if (instructions.activeInHierarchy)
+        {
+            instructions.SetActive(false);
+        }
+        else
+        {
+            instructions.SetActive(true);
+        }
+    }
+}
